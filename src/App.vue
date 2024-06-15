5aa6769fe2e9164b59090ac9b19e5b1ee3b9bf3d
kidchang93 <lck930721@gmail.com>
2024-06-15 21:04:46 +0900

    mutations 사용방법

--- src/App.vue
+++ src/App.vue
@@ -13,8 +13,9 @@
 <!--  vuex 사용법-->
   <h4> 안녕 {{ $store.state.name }}</h4>
 <!--  아래처럼 사용하면 안된다. -->
-  <button @click="$store.state.name = '이' ">버튼</button>
-
+<!--  <button @click="$store.state.name = '이' ">버튼</button>-->
+<!-- 이렇게 짜자  store에게 부탁해서 state가 변경하게 한다. -->
+  <button @click="$store.commit('changeName')">버튼</button>
   <Container :postingData="postingData"
              :step="step"
              :image="image"
--- src/store.js
+++ src/store.js
@@ -4,8 +4,16 @@ const store = createStore({
     state(){
         return {
             name: 'lee',
+            age: 31,
         }
     },
+    // 데이터 수정하는 방법을 미리 정의
+    mutations:{
+      changeName(state){
+          state.name = 'park'
+      }
+    },
+
 })
 
 export default store
